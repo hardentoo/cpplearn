92079ef34f3e85c5e1d0576585be3c873f5a4dd3
hardentoo <hardenedgentoo@gmail.com>
2017-03-24 00:57:17 +0300

    Add some examples

--- base/hello.cpp
+++ base/hello.cpp
@@ -1,10 +1,35 @@
+#include <cstdlib>
 #include <iostream>
+#include <unistd.h>
+
+#ifdef _WIN32
+#include <conio.h>
+#endif
 
 using namespace std;
 
 int main (){
 
     cout << "Hello, world!" << endl;
+
+    #ifdef __linux
+
+    cout << "Linux" << endl;
+
+    //getchar();
+    //sleep(3);
+    pause();
+
+    #elif _WIN32
+
+    cout << "Windows" << endl;
+
+    //getchar();
+    while (1) if (kbhit()) break; 
+
+    #else
+
+    #endif
 
     return 0;
 }
